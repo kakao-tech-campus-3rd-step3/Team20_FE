801a72b08b82e236faf21ef31a1650c1ee51c4a1
Jaeho <162589155+Jaeho-Site@users.noreply.github.com>
2025-08-31 21:23:33 +0900

    chore : project setting (#5)

---
+++ src/entities/content/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './Grandparent';
+
+// 캡슐화 합니다. 상위 layer에서 조립할 최상위 컴포넌트만 expot, 필요하다면 그때 export
+//경로는 무조건 alias로 작성합니다. -> 캡슐화의 이유
+//export * from './Parent';
+//export * from './Child';
