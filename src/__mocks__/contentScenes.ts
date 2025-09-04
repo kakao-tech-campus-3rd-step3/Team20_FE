08c611760ad2052159616069608a1304aa0f6c72
Jaeho <sjaeho348@gmail.com>
2025-09-04 14:43:56 +0900

    feat : Content페이지 LocationImageCarousel Mock 데이터 생성

---
+++ src/__mocks__/contentScenes.ts
@@ -0,0 +1,42 @@
+import squidgameSpotdetail from './squidgame-spotdetail.jpg';
+import squidgameSpotdetail2 from './squidgame-spotdetail2.jpg';
+import squidgameSpotdetail3 from './squidgame-spotdetail3.png';
+import squidgameSpotdetail4 from './squidgame-spotdetail4.png';
+
+export const contentScenes = [
+  {
+    id: 1,
+    title: '무궁화 꽃이 피었습니다',
+    description:
+      '456명의 참가자들이 참여한 첫 번째 게임. 어린 시절 추억의 놀이가 생과 사를 가르는 잔혹한 게임으로 변했다.',
+    image: squidgameSpotdetail,
+    episode: '1화',
+    timestamp: '32:15',
+  },
+  {
+    id: 2,
+    title: '설탕 뽑기',
+    description:
+      '달고나에서 모양을 온전히 뽑아내야 하는 두 번째 게임. 단순해 보이지만 가장 어려운 게임 중 하나.',
+    image: squidgameSpotdetail2,
+    episode: '3화',
+    timestamp: '15:30',
+  },
+  {
+    id: 3,
+    title: '줄다리기',
+    description: '팀워크가 중요한 세 번째 게임. 전략과 협력이 승패를 가르는 게임.',
+    image: squidgameSpotdetail3,
+    episode: '4화',
+    timestamp: '28:45',
+  },
+  {
+    id: 4,
+    title: '오징어 게임',
+    description:
+      '마지막 게임이자 드라마의 제목이 된 게임. 어린 시절의 추억과 현실의 잔혹함이 만나는 순간.',
+    image: squidgameSpotdetail4,
+    episode: '9화',
+    timestamp: '45:20',
+  },
+];
