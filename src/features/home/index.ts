3b85e351aab05d43ac45283b8c06c953ebe5d197
leehuiseok <leehuiseok020412@gmail.com>
2025-09-04 18:21:12 +0900

    feat: export index.ts 작성

---
+++ src/features/home/index.ts
@@ -0,0 +1,5 @@
+export * from '@/features/home/ui/Hero';
+export * from '@/features/home/ui/CategorySection';
+export * from '@/features/home/ui/PopularCarousel';
+export * from '@/features/home/ui/SpotPreview';
+export * from '@/features/home/ui/PopularRoutes';
