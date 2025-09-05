1f73577bed8dff133a28dd5a485d1ff0a76a6c9d
leehuiseok <leehuiseok020412@gmail.com>
2025-09-05 16:37:23 +0900

    refactor: 필요없는 파일제거

--- src/features/home/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '@/features/home/ui/Hero';
-export * from '@/features/home/ui/CategorySection';
-export * from '@/features/home/ui/PopularCarousel';
-export * from '@/features/home/ui/SpotPreview';
-export * from '@/features/home/ui/PopularRoutes';
