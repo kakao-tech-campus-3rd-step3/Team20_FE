a466db73e38a75337f56833488da472b91033bcf
Jaeho <sjaeho348@gmail.com>
2025-09-07 19:10:03 +0900

    feat : router 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
+
 function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-gray-900">
-      <h1 className="text-3xl font-bold text-white underline decoration-sky-500">
-        충남대 2팀 앞으로 화이팅입니다 <br />
-        README.md 간단하게 읽어주세요!
-      </h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
