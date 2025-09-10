8561c26dd9015c1229a309dd85c9ebdd9ada7908
Jaeho <sjaeho348@gmail.com>
2025-09-11 01:18:14 +0900

    refactor : 모듈 export수정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,4 +2,4 @@
 // TODO : shared에서 컴포넌트를 정의한 이상, 이 컴포넌트가 사용될 수 있는 상위 컴포넌트가 있다면
 // 무조건 shared컴포넌트를 재사용해주셔야 해요! import경로도 앞으로@/shared/ui/로 해주세요!
 export { IconButton } from './IconButton/IconButton';
-export { Button, buttonVariants } from './Button/Button';
+export { Button } from './Button/Button';
