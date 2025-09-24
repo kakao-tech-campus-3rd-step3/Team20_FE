ad86b8c88192f8bac4b914ef22b6e9b24a5b5005
Jaeho <sjaeho348@gmail.com>
2025-09-24 17:21:24 +0900

    chore : Tanstack Router 의존성 설치, 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import TanStackRouterVite from '@tanstack/router-plugin/vite';
 
-// https://vite.dev/config/
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
@@ -19,10 +18,11 @@ export default defineConfig({
       {
         extends: true,
         plugins: [
-          // The plugin will run tests for the stories defined in your Storybook config
-          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
             configDir: path.join(dirname, '.storybook'),
+          }),
+          TanStackRouterVite({
+            routesDirectory: './src/pages',
           }),
         ],
         test: {
