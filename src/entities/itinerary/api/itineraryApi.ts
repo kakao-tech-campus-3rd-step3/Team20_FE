1270cb0e43297be61cf76b6fe2a8f6613e05ef4f
shushuburger <03robin@naver.com>
2025-10-29 21:34:03 +0900

    refactor: 동선 api be 연동

--- src/entities/itinerary/api/itineraryApi.ts
+++ src/entities/itinerary/api/itineraryApi.ts
@@ -9,16 +9,13 @@ import type {
 export const createItinerary = async (
   data: CreateItineraryRequest,
 ): Promise<CreateItineraryResponse> => {
-  const response = await http.post<CreateItineraryResponse>('/itineraries', data);
-  return response.data;
+  return (await http.post('/itineraries', data)) as CreateItineraryResponse;
 };
 
 export const getItineraries = async (): Promise<ItinerariesListResponse> => {
-  const response = await http.get<ItinerariesListResponse>('/itineraries');
-  return response.data;
+  return (await http.get('/itineraries')) as ItinerariesListResponse;
 };
 
 export const getItineraryDetail = async (itineraryId: string): Promise<ItineraryDetailResponse> => {
-  const response = await http.get<ItineraryDetailResponse>(`/itineraries/${itineraryId}`);
-  return response.data;
+  return (await http.get(`/itineraries/${itineraryId}`)) as ItineraryDetailResponse;
 };
