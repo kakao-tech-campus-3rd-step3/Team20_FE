c4c065ce5528d98558fbe9c6dc41819a1b92b1ca
Jaeho <sjaeho348@gmail.com>
2025-10-31 01:05:14 +0900

    feat : 동선 삭제 api 함수 구현

--- src/entities/itinerary/api/itineraryApi.ts
+++ src/entities/itinerary/api/itineraryApi.ts
@@ -19,3 +19,7 @@ export const getItineraries = async (): Promise<ItinerariesListResponse> => {
 export const getItineraryDetail = async (itineraryId: string): Promise<ItineraryDetailResponse> => {
   return (await httpBackend.get(`/itineraries/${itineraryId}`)) as ItineraryDetailResponse;
 };
+
+export const deleteItinerary = async (itineraryId: string): Promise<void> => {
+  await httpBackend.delete(`/itineraries/${itineraryId}`);
+};
