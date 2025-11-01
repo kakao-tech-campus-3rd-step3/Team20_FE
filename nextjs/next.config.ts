cd742bb7fe02301ca57e6ab8686dc1dd5b57c7e7
Jaeho <sjaeho348@gmail.com>
2025-11-02 03:52:56 +0900

    chore : next image설정 변경

--- nextjs/next.config.ts
+++ nextjs/next.config.ts
@@ -23,7 +23,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: false,
-    contentDispositionType: 'attachment',
     remotePatterns: [
       {
         protocol: 'https',
